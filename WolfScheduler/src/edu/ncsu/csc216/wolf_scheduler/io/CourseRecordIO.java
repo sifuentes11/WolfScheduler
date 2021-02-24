e1448fa87c7931ee5c0d86d216bf66ce54f37547
sifuentes11 <armandos@live.unc.edu>
2021-02-23 19:33:34 -0500

    Added CourseRecordIO and CourseRecordIOTest.

---
+++ WolfScheduler/src/edu/ncsu/csc216/wolf_scheduler/io/CourseRecordIO.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package edu.ncsu.csc216.wolf_scheduler.io;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import edu.ncsu.csc216.wolf_scheduler.course.Course;
+
+/**
+ * @author Sifuentes11
+ *
+ */
+public class CourseRecordIO {
+
+	public static ArrayList<Course> readCourseRecords(String invalidTestFile) throws FileNotFoundException{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static void writeCourseRecords(String fileName, ArrayList<Course> courses) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
