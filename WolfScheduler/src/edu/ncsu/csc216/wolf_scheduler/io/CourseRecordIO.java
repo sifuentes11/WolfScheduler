ffcf2ab794483dc4b6f3585519788b8626988504
sifuentes11 <armandos@live.unc.edu>
2021-02-23 20:34:15 -0500

    Added scanner to read files including course information.

--- WolfScheduler/src/edu/ncsu/csc216/wolf_scheduler/io/CourseRecordIO.java
+++ WolfScheduler/src/edu/ncsu/csc216/wolf_scheduler/io/CourseRecordIO.java
@@ -3,9 +3,13 @@
  */
 package edu.ncsu.csc216.wolf_scheduler.io;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import edu.ncsu.csc216.wolf_scheduler.course.Course;
 
@@ -15,13 +19,51 @@ import edu.ncsu.csc216.wolf_scheduler.course.Course;
  */
 public class CourseRecordIO {
 
-	public static ArrayList<Course> readCourseRecords(String invalidTestFile) throws FileNotFoundException{
+	public static ArrayList<Course> readCourseRecords(String fileName) throws FileNotFoundException{
+		// TODO Auto-generated method stub
+		
+		Scanner fileReader = new Scanner( new FileInputStream(fileName));
+		ArrayList<Course> courses = new ArrayList<Course>();
+		
+		
+		while (fileReader.hasNextLine()) {
+			try {
+				Course course = readCourse(fileReader.nextLine());
+				boolean duplicate = false;
+				for (int i = 0; i < courses.size(); i++) {
+					Course c = courses.get(i);
+					if (course.getName().equals(c.getName()) && course.getSection().equals(c.getSection())) {
+						duplicate = true;
+					}
+				}
+				if (!duplicate) {
+					courses.add(course);
+				}
+			} catch (IllegalArgumentException e) {
+
+			}
+		}
+
+		fileReader.close();
+
+		return courses;
+	}
+
+	private static Course readCourse(String nextLine) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	public static void writeCourseRecords(String fileName, ArrayList<Course> courses) throws IOException {
 		// TODO Auto-generated method stub
+		
+		PrintStream fileWriter = new PrintStream(new File(fileName));
+		
+		for (int i = 0; i < courses.size(); i++) {
+			fileWriter.println(courses.get(i).toString());
+		}
+		
+		fileWriter.close();
 		
 	}
 
